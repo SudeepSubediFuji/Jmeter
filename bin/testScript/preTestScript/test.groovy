905b7ba5dd13c1d8f599cd3e0b8a8bcbd30f0f05
SudeepSubediFuji <subedi.sudeep@fujisystems.co.jp>
2026-03-13 18:06:08 +0900

    groovyの前処理や後処理の追加

---
+++ bin/testScript/preTestScript/test.groovy
@@ -0,0 +1,6 @@
+log.info("Start of preTestScript")
+// Cannot invoke the prev method as the pre test is run before it .
+// log.info("Groovy previous response code "+ prev.getResponseCode())
+
+
+log.info("End of preTestScript")
